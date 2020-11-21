99470caf931b5465d95a4ca14c96bc8076eb8689
nasgarzada <nijat.asgarzada@pashabank.az>
2020-11-22 03:56:04 +0400

    implement get recipes

--- src/main/java/io/course/receipe/controllers/IndexController.java
+++ src/main/java/io/course/receipe/controllers/IndexController.java
@@ -1,14 +1,20 @@
 package io.course.receipe.controllers;
 
+import io.course.receipe.services.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private RecipeService recipeService;
+
     @GetMapping("")
-    public String getIndexPage(){
-//        System.out.println("Getting....");
+    public String getIndexPage(Model model) {
+        model.addAttribute("recipes",recipeService.getRecipes());
         return "index";
     }
 }
