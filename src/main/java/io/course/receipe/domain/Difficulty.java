584f8b525c4b2b5c974db9f8f858ed71ca9109b0
nasgarzada <nijat.asgarzada@pashabank.az>
2020-11-22 02:16:08 +0400

    Add recipe database design

---
+++ src/main/java/io/course/receipe/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package io.course.receipe.domain;
+
+public enum Difficulty {
+    EASY, MODERATE, KIND_OF_HARD,HARD;
+}
---
+++ src/main/java/io/course/receipe/domain/Ingredient.java
@@ -0,0 +1,28 @@
+package io.course.receipe.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure unitOfMeasure;
+
+    @ManyToOne
+    private Recipe recipe;
+}
